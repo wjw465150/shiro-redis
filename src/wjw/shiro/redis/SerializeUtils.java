00673dd0961aa65f4fe7a43286069ff9f5267269
wjw465150 <wjw465150@gmail.com>
2015-12-24 16:44:28 +0800

    采用:kryo3来序列化!

--- src/wjw/shiro/redis/SerializeUtils.java
+++ src/wjw/shiro/redis/SerializeUtils.java
@@ -1,13 +1,8 @@
 package wjw.shiro.redis;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wjw.kryo.wrapper.KryoSerializer;
 
 public class SerializeUtils {
 
@@ -27,19 +22,7 @@ public class SerializeUtils {
     }
 
     try {
-      ByteArrayInputStream byteStream = new ByteArrayInputStream(bytes);
-      try {
-        ObjectInputStream objectInputStream = new ObjectInputStream(byteStream);
-        try {
-          result = objectInputStream.readObject();
-        } catch (ClassNotFoundException ex) {
-          throw new Exception("Failed to deserialize object type", ex);
-        } finally {
-          objectInputStream.close();
-        }
-      } catch (Throwable ex) {
-        throw new Exception("Failed to deserialize", ex);
-      }
+      result = KryoSerializer.read(bytes);
     } catch (Exception e) {
       logger.error("Failed to deserialize", e);
     }
@@ -58,25 +41,9 @@ public class SerializeUtils {
     if (object == null) {
       return new byte[0];
     }
+
     try {
-      ByteArrayOutputStream byteStream = new ByteArrayOutputStream(128);
-      ObjectOutputStream objectOutputStream = null;
-      try {
-        if (!(object instanceof Serializable)) {
-          throw new IllegalArgumentException(SerializeUtils.class.getSimpleName() + " requires a Serializable payload " +
-              "but received an object of type [" + object.getClass().getName() + "]");
-        }
-        objectOutputStream = new ObjectOutputStream(byteStream);
-        objectOutputStream.writeObject(object);
-        objectOutputStream.flush();
-        result = byteStream.toByteArray();
-      } catch (Throwable ex) {
-        throw new Exception("Failed to serialize", ex);
-      } finally {
-        if (objectOutputStream != null) {
-          objectOutputStream.close();
-        }
-      }
+      result = KryoSerializer.write(object);
     } catch (Exception ex) {
       logger.error("Failed to serialize", ex);
     }
